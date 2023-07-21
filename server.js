50516bc385adb0d8374cd948b69e25f898431d57
meryem <meryemajmani33@gmail.com>
2023-07-21 01:29:58 +0100

    get request and axios

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
-const env=require("dotenv").config();
+const axios = require('axios');
+const env = require("dotenv").config();
+
 app.use(express.json());
 
+app.get('/search-ing', async (req, res) => {
+  const { ing } = req.body;
+  
+  const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
 
+  const payload = {
+    ingr: ing,
+  };
 
-app.listen(5000, () => {
-  console.log('Server is running ');
+  try {
+    const response = await axios.get(url, payload);
+    const data = response.data;
+    // console.log(data);
+    console.log(data.hints.length);
+    res.json(data.hints);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(error.response ? error.response.status : 500).json({ error: 'Something went wrong' });
+  }
 });
 
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
